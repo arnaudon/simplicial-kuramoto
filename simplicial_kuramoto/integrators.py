8b1bf2b5844c17df3eb5f9c4de185ba3a8e1a875
paex <paul.expert@gmail.com>
2020-04-07 21:17:14 +0100

    Starting to incorporate old functions
    Not everything is homogenised in term of notation/syntax, consider this an alpha addition:
    - Edge-integrator in integratotr.py
    - Advanced plotting function in plotting.py
    - Edge incidence matrix in SimplicialComplex.py

--- simplicial_kuramoto/integrators.py
+++ simplicial_kuramoto/integrators.py
@@ -1,4 +1,4 @@
-"""Numerical integratiors."""
+"""Numerical integrators."""
 from functools import partial
 
 import numpy as np
@@ -6,7 +6,7 @@ from scipy.integrate import solve_ivp
 
 
 def node_simplicial_kuramoto(time, phase, simplicial_complex=None, omega_0=None):
-    """Node simplicail kuramoto, or classical Kuramoto."""
+    """Node simplicial kuramoto, or classical Kuramoto."""
     B0 = simplicial_complex.node_incidence_matrix
     W1 = simplicial_complex.edge_weight_matrix
     degree = simplicial_complex.degree
@@ -34,3 +34,45 @@ def integrate_node_kuramoto(
         rtol=1.0e-8,
         atol=1.0e-8,
     )
+
+
+def edge_simplicial_kuramoto(time, phase, simplicial_complex=None, omega_0=None, a=1):
+    """Edge simplicial kuramoto"""
+    
+    ## a is the "equivalent" of degree, not the overall normalisation needs to be rethought
+
+    B0 = simplicial_complex.node_incidence_matrix
+    B1 = simplicial_complex.edge_incidence_matrix
+    W1 = simplicial_complex.edge_weight_matrix
+    W2 = simplicial_complex.face_weight_matrix
+    degree = simplicial_complex.degree
+    
+    if omega_0 is None:
+        omega_0 = np.zeros(simplicial_complex.n_edges)
+    
+    return omega_0-(B0.dot(np.sin(B0.T.dot(theta))))-a*B1.T.dot(np.sin(B1.dot(theta)))
+#     this is the line for weighted version
+#     return omega_0-a*np.diag(W1).dot(B0.dot(np.sin(B0.T.dot(theta))))-a*B1.T.dot(np.diag(W2).dot(np.sin(B1.dot(theta))))
+
+
+def integrate_simplicial_kuramoto(
+    simplicial_complex, initial_phase, t_max, n_t, omega_0=None, a
+):#, weights_e, weights_f):
+    """Integrate the edge Kuramoto model."""
+    
+    rhs = partial(
+        edge_simplicial_kuramoto, simplicial_complex=simplicial_complex, omega_0=omega_0,a=1
+    )    
+    t_eval = np.linspace(0, t_max, n_t)
+    
+    return solve_ivp(
+        rhs,
+        # lambda t, theta: simplicial_kuramoto_full_theta(t, theta, B0, B1, omega_0, a) // old version
+        [0, t_max],
+        intial_phase, 
+        method='Radau', ## We use Radau instead of LSODA, I'm not sure why, but I think it was because of some convergence issue
+        rtol=1.49012e-8,
+        atol=1.49012e-8,
+        )
+
+#     return integ.solve_ivp(lambda t, theta: simplicial_kuramoto_full_theta(t, theta, B0, B1, a, omega_0, degree, weights_n, weights_e, weights_f), [0, t_max], theta_0, t_eval = np.linspace(0, t_max, n_t),method='Radau',rtol=1.49012e-8,atol=1.49012e-8)
--- simplicial_kuramoto/plotting.py
+++ simplicial_kuramoto/plotting.py
@@ -8,3 +8,45 @@ def plot_node_kuramoto(node_results):
     plt.imshow(node_results.y, aspect="auto")
     plt.xlabel("time")
     plt.ylabel("mode id")
+
+def plot_flow(initial_phase,simpicial_somplex,result,plotname=None):
+    """ Some outputs (curl, div, etc ...) are only useful for understanding what is happening """
+    times = result.t
+    phase = result.y
+    
+    B0=simplicial_complex.node_incidence_matrix
+    B1=simplicial_complex.edge_incidence_matrix
+    
+    plt.figure()
+    plt.imshow(np.mod(np.around(phase,10),np.around(2*np.pi,10)), aspect='auto',cmap='bwr')
+    plt.title(plotname+' phases')
+    plt.colorbar()
+    
+    op=order_parameter(phase, 4, 1) # that is in the utils.py
+    plt.figure()
+    plt.title(plotname+' order parameter')
+    plt.plot(op[0,:])
+
+    print('\theta_0: ', initial_phase)
+    print('\theta_final: ',phase[:,-1])
+    print('\theta_final: ',np.mod(np.around(phase[:,-1],10),np.around(2*np.pi,10)))
+    
+    Div=np.mod(np.around(B0.T.dot(phase),10),np.around(2*np.pi,10))
+    Curl=np.mod(np.around(B1.dot(phase),10),np.around(2*np.pi,10))
+    print('Div: ', Div[:,-1])
+    print('Curl: ', Curl[:,-1])
+    
+    L1=-B0.dot(B0.T)-B1.T.dot(B1)
+    print('L1\theta: ', L1.dot(phase[:,-1]))
+    print('L1\theta: ', np.mod(np.around(L1.dot(phase[:,-1]),10),np.around(2*np.pi,10)))
+    print('dim(Ker(L1)): ', null_space(L1).shape[1])
+    print('Ker(L1): ', null_space(L1))
+    
+#     plt.figure()
+#     plt.imshow(Div, aspect='auto',cmap='bwr')
+#     plt.title(plotname+' divergence')
+#     plt.colorbar()
+#     plt.figure()
+#     plt.imshow(Curl, aspect='auto',cmap='bwr')
+#     plt.title(plotname+' curl')
+#     plt.colorbar()
