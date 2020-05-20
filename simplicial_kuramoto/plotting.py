97a47326a44acbd2c80bd9f1e40bbf992bb6a203
paex <paul.expert@gmail.com>
2020-05-20 22:29:50 +0100

    Plots and Example
    Added a couple of plotting function
    Including one for the order parameter of the phase
    Added an example with a square and a diagonal:
    full-full,full-empty,empty-empty

--- simplicial_kuramoto/plotting.py
+++ simplicial_kuramoto/plotting.py
@@ -25,8 +25,8 @@ def plot_edge_kuramoto(edge_results):
     """Basic plot for edge kuramoto."""
     plt.figure()
     plt.imshow(
-        edge_results.y,
-        # np.mod(np.around(edge_results.y, 10), np.around(2 * np.pi, 10)),
+        # edge_results.y,
+        np.mod(np.around(edge_results.y, 10), np.around(2 * np.pi, 10)),
         aspect="auto",
         cmap="bwr",
         extent=(edge_results.t[0], edge_results.t[-1], 0, len(edge_results.y)),
@@ -42,38 +42,63 @@ def plot_flow(initial_phase, simplicial_complex, result, plotname="Test"):
 
     B0 = simplicial_complex.node_incidence_matrix
     B1 = simplicial_complex.edge_incidence_matrix
-
+    
     # op=order_parameter(phase, 4, 1) # that is in the utils.py
     # plt.figure()
     # plt.title('Order parameter')
     # plt.plot(op[0,:])
 
-    print("\theta_0: ", initial_phase)
-    print("\theta_final: ", phase[:, -1])
-    print(
-        "\theta_final: ", np.mod(np.around(phase[:, -1], 10), np.around(2 * np.pi, 10))
-    )
+    if(B1==None):
+        print("theta_0: ", initial_phase)
+        print("theta_final: ", phase[:, -1])
+        print(
+            "theta_final: ", np.mod(np.around(phase[:, -1], 10), np.around(2 * np.pi, 10))
+        )
 
-    Div = np.mod(np.around(B0.T.dot(phase), 10), np.around(2 * np.pi, 10))
-    Curl = np.mod(np.around(B1.dot(phase), 10), np.around(2 * np.pi, 10))
-    print("Div: ", Div[:, -1])
-    print("Curl: ", Curl[:, -1])
+        Div = np.mod(np.around(B0.T.dot(phase), 10), np.around(2 * np.pi, 10))
+        print("Div: ", Div[:, -1])
+        print("Curl: no curl")
 
-    L1 = -B0.dot(B0.T) - B1.T.dot(B1)
-    print("L1\theta: ", L1.dot(phase[:, -1]))
-    print(
-        "L1\theta: ",
-        np.mod(np.around(L1.dot(phase[:, -1]), 10), np.around(2 * np.pi, 10)),
-    )
-    
-    #w, v=eigs(L1)
-    #ns_ind=np.where(w<10e-8)
-    #print("dim(Ker(L1)): ", len(ns_ind)
-    
-    ns=null_space(L1.toarray()) # not ideal, would be better in principle to stay in sparse representation, but scipy.sparse.linalg.eigs does not work
-    print("dim(Ker(L1)): ", ns.shape[1])
-    print("Ker(L1): ", ns)
+        L1 = -B0.dot(B0.T)
+        print("L1 theta: ", L1.dot(phase[:, -1]))
+        print(
+            "L1 theta: ",
+            np.mod(np.around(L1.dot(phase[:, -1]), 10), np.around(2 * np.pi, 10)),
+        )
 
+        #w, v=eigs(L1)
+        #ns_ind=np.where(w<10e-8)
+        #print("dim(Ker(L1)): ", len(ns_ind)
+
+        ns=null_space(L1.toarray()) # not ideal, would be better in principle to stay in sparse representation, but scipy.sparse.linalg.eigs does not work
+        print("dim(Ker(L1)): ", ns.shape[1])
+        print("Ker(L1): ", ns)
+    else:
+        print("theta_0: ", initial_phase)
+        print("theta_final: ", phase[:, -1])
+        print(
+            "theta_final: ", np.mod(np.around(phase[:, -1], 10), np.around(2 * np.pi, 10))
+        )
+        
+        Div = np.mod(np.around(B0.T.dot(phase), 10), np.around(2 * np.pi, 10))
+        Curl = np.mod(np.around(B1.dot(phase), 10), np.around(2 * np.pi, 10))
+        print("Div: ", Div[:, -1])
+        print("Curl: ", Curl[:, -1])
+
+        L1 = -B0.dot(B0.T) - B1.T.dot(B1)
+        print("L1 theta: ", L1.dot(phase[:, -1]))
+        print(
+            "L1 theta: ",
+            np.mod(np.around(L1.dot(phase[:, -1]), 10), np.around(2 * np.pi, 10)),
+        )
+
+        #w, v=eigs(L1)
+        #ns_ind=np.where(w<10e-8)
+        #print("dim(Ker(L1)): ", len(ns_ind)
+
+        ns=null_space(L1.toarray()) # not ideal, would be better in principle to stay in sparse representation, but scipy.sparse.linalg.eigs does not work
+        print("dim(Ker(L1)): ", ns.shape[1])
+        print("Ker(L1): ", ns)
 
 #     plt.figure()
 #     plt.imshow(Div, aspect='auto',cmap='bwr')
@@ -83,3 +108,24 @@ def plot_flow(initial_phase, simplicial_complex, result, plotname="Test"):
 #     plt.imshow(Curl, aspect='auto',cmap='bwr')
 #     plt.title(plotname+' curl')
 #     plt.colorbar()
+
+def plot_order_parameter(phases,return_op=False,plot=True):
+    N=phases.shape[0]
+    op = np.zeros((phases.shape[1]))
+    op = np.absolute(np.exp(1j*phases).sum(0))/N
+    if(plot):
+        plt.figure()
+        plt.plot(op)
+        plt.title(op[-1])
+        plt.show()
+    if(return_op):
+        return op
+
+def plot_unit_circle(phases):
+    t=np.linspace(0,2*np.pi,1000)
+    plt.figure()
+    plt.plot(np.cos(t), np.sin(t),'b', linewidth=1)
+    plt.plot([0,0],[-1,1],'b-.', linewidth=0.5)
+    plt.plot([-1,1],[0,0],'b-.', linewidth=0.5)
+    plt.plot(np.cos(phases[:,-1]),np.sin(phases[:,-1]),'o')
+    plt.show()
