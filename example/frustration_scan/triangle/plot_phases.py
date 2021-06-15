a0464bbebbbc13d5ff2f9bf144aa432db49e6c40
arnaudon <alexis.arnaudon@epfl.ch>
2021-06-15 22:05:02 +0200

    c-d transition

--- example/frustration_scan/triangle/plot_phases.py
+++ example/frustration_scan/triangle/plot_phases.py
@@ -30,19 +30,29 @@ def plot_phase_traj(Gsc, alpha_1, alpha_2, folder="figures_traj", t_max=50, min_
 
     print(f"grad slope = {grad_slope}, curl slope = {curl_slope}")
 
-    plt.figure(figsize=(4,4))
+    plt.figure(figsize=(4, 4))
 
     plt.scatter(np.sin(result[0]), np.sin(result[2]), c=grad, s=10 * curl + min_s)
-    #plt.colorbar(label="gradient slope")
+    # plt.colorbar(label="gradient slope")
     plt.axis([-1.01, 1.01, -1.01, 1.01])
-    plt.axis('equal')
-    plt.savefig(f"{folder}/traj_a1_{alpha_1}_a2_{alpha_2}.pdf", bbox_inches="tight")
+    plt.axis("equal")
+    plt.suptitle(f"alpha_1 = {alpha_1}, alpha2 = {alpha_2}")
+    plt.savefig(
+        f"{folder}/traj_a1_{np.round(alpha_1, 5)}_a2_{np.round(alpha_2, 3)}.pdf",
+        bbox_inches="tight",
+    )
+    plt.close()
 
     plt.figure()
     plt.plot(time, grad, label="grad")
     plt.plot(time, curl, label="curl")
     plt.legend(loc="best")
-    plt.savefig(f"{folder}/proj_a1_{alpha_1}_a2_{alpha_2}.pdf", bbox_inches="tight")
+    plt.suptitle(f"alpha_1 = {alpha_1}, alpha2 = {alpha_2}")
+    plt.savefig(
+        f"{folder}/proj_a1_{np.round(alpha_1, 5)}_a2_{np.round(alpha_2, 3)}.pdf",
+        bbox_inches="tight",
+    )
+    plt.close()
 
 
 if __name__ == "__main__":
@@ -66,3 +76,7 @@ if __name__ == "__main__":
     alpha_1 = 1.2
     alpha_2 = 2.5
     plot_phase_traj(Gsc, alpha_1, alpha_2, t_max=18, min_s=5)
+
+    for alpha_1 in np.linspace(1.193, 1.195, 20):
+        alpha_2 = 1.5
+        plot_phase_traj(Gsc, alpha_1, alpha_2, t_max=50, min_s=5, folder="figures_traj_scan")
