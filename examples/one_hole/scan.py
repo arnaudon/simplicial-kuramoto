42384e140c3509e1ac895149fa43d01f7c6508b0
Alexis Arnaudon <alexis.arnaudon@epfl.ch>
2021-11-09 09:23:07 +0100

    clenup example folder

--- examples/one_hole/scan.py
+++ examples/one_hole/scan.py
@@ -1,26 +1,9 @@
+"""Scan frustration parameter."""
 import numpy as np
-import sys
-from copy import copy
-import matplotlib.pyplot as plt
 import networkx as nx
-import itertools as it
-import pickle
-from tqdm import tqdm
-import pandas as pd
-import copy
-from numpy import linalg as LA
 
-from simplicial_kuramoto.integrators import integrate_edge_kuramoto
 from simplicial_kuramoto import SimplicialComplex
-from simplicial_kuramoto.frustration_scan import scan_frustration_parameters, proj_subspace
-from simplicial_kuramoto.graph_generator import delaunay_with_holes
-from simplicial_kuramoto.frustration_scan import (
-    get_subspaces,
-    compute_order_parameter,
-    plot_order_1d,
-    _get_projections_1d,
-)
-from simplicial_kuramoto.plotting import draw_simplicial_complex
+from simplicial_kuramoto.frustration_scan import scan_frustration_parameters
 
 if __name__ == "__main__":
     t_max = 1000
@@ -29,7 +12,6 @@ if __name__ == "__main__":
     repeats = 10
     n_alpha2 = 200
 
-    # The candidate
     G = nx.Graph()
 
     G.add_edge(0, 1, weight=1, edge_com=0)
@@ -65,28 +47,13 @@ if __name__ == "__main__":
     Gsc = SimplicialComplex(graph=G, no_faces=False)
 
     del Gsc.faces[3]
-
-    ## original orientation
-    scan_frustration_parameters(
-        Gsc,
-        folder="./results/",
-        filename=f"Fig_2_example_1_1.pkl",
-        alpha1=[0.0],
-        alpha2=np.linspace(0, np.pi / 2.0, n_alpha2),
-        repeats=repeats,
-        n_workers=n_workers,
-        t_max=t_max,
-        n_t=n_t,
-        harmonic=True,
-    )
-
-    ## with edge 6 flipped
+    # with edge 6 flipped
     Gsc.flip_edge_orientation(6)
 
     scan_frustration_parameters(
         Gsc,
         folder="./results/",
-        filename=f"Fig_2_example_1_2.pkl",
+        filename="complex_0.pkl",
         alpha1=[0.0],
         alpha2=np.linspace(0, np.pi / 2.0, n_alpha2),
         repeats=repeats,
@@ -96,13 +63,13 @@ if __name__ == "__main__":
         harmonic=True,
     )
 
-    ## with edge 6 and 5 flipped
+    # with edge 6 and 5 flipped
     Gsc.flip_edge_orientation(5)
 
     scan_frustration_parameters(
         Gsc,
         folder="./results/",
-        filename=f"Fig_2_example_1_3.pkl",
+        filename="complex_1.pkl",
         alpha1=[0.0],
         alpha2=np.linspace(0, np.pi / 2.0, n_alpha2),
         repeats=repeats,
@@ -112,30 +79,13 @@ if __name__ == "__main__":
         harmonic=True,
     )
 
-    ## with edge 6, 5 and 4 flipped
+    # with edge 6, 5 and 4 flipped
     Gsc.flip_edge_orientation(4)
 
     scan_frustration_parameters(
         Gsc,
         folder="./results/",
-        filename=f"Fig_2_example_1_4.pkl",
-        alpha1=[0.0],
-        alpha2=np.linspace(0, np.pi / 2.0, n_alpha2),
-        repeats=repeats,
-        n_workers=n_workers,
-        t_max=t_max,
-        n_t=n_t,
-        harmonic=True,
-    )
-
-    ## with edge 6, 5 and 9 flipped
-    Gsc.flip_edge_orientation(4)
-    Gsc.flip_edge_orientation(9)
-
-    scan_frustration_parameters(
-        Gsc,
-        folder="./results/",
-        filename=f"Fig_2_example_1_5.pkl",
+        filename="complex_2.pkl",
         alpha1=[0.0],
         alpha2=np.linspace(0, np.pi / 2.0, n_alpha2),
         repeats=repeats,
